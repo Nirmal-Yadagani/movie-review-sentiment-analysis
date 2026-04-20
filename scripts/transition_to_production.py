6a47ed481b493e3fd867150c6abc6d67b6e59370
Nirmal Yadagani <nirmalproffesionallife@gmail.com>
2026-04-20 16:56:24 +0530

    Implimented transition_to_production.py to automaticaly change alias to production.

---
+++ scripts/transition_to_production.py
@@ -0,0 +1,37 @@
+import sys
+import dagshub
+from mlflow.tracking import MlflowClient
+from dotenv import load_dotenv
+
+load_dotenv()
+dagshub.init(repo_owner='Nirmal-Yadagani', repo_name='movie-review-sentiment-analysis', mlflow=True)
+
+def transition_staging_to_production(model_name):
+    client = MlflowClient()
+    try:
+        # Find which version currently holds the "Staging" alias
+        staging_model = client.get_model_version_by_alias(name=model_name, alias="Staging")
+        version_to_promote = staging_model.version
+        
+        print(f"Found Staging model (Version {version_to_promote}). Promoting to Production...")
+        
+        # Apply the Production alias to this version
+        client.set_registered_model_alias(
+            name=model_name, 
+            alias="Production",
+            version=version_to_promote
+        )
+        
+        # Optionally, remove the Staging alias so it's clean for the next run
+        client.delete_registered_model_alias(name=model_name, alias="Staging")
+        
+        print(f"Successfully transitioned Version {version_to_promote} to Production!")
+
+    except Exception as e:
+        print(f"Error transitioning model: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    # Ensure this matches your params.yaml register_model_name
+    model_name = "MLPClassifier" 
+    transition_staging_to_production(model_name)
